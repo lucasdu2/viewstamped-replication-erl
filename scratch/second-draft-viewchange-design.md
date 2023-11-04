6cc9665532208300d7765fefb74b57ee31f695c1
Lucas Du <lucasdu@pop-os.localdomain>
2023-11-03 19:30:28 -0700

    Start second attempt at viewchange protocol

---
+++ scratch/second-draft-viewchange-design.md
@@ -0,0 +1,61 @@
+# View change protocol
+Let's try this again, now that you've had a chance to grapple with the protocol
+for a bit. Here's a second attempt at organizing the code structure in service
+of the view change protocol. 
+
+## General philosophy
+Everything revolves around replica state. Within the replica state, arguably the
+most important piece is the log. The state is the source of truth for what is
+happening in the system. At the beginning of every execution loop, each replica
+should look to its state to understand its current role and status in the system.
+
+## Common structures
+- Replica state should be contained in a record. You should also add all the 
+state necessary to handle view changes into the common replica state--this will
+save us from having to pass 2 separate pieces of state around.
+- Messages should also be records. When sending a message, we should also prefix
+it with the Name of the sender--perhaps it would also be more efficient to
+prefix it with an atom denoting the message type as well, so we don't have to
+do a record type check. 
+- We should use the Name of the sender to send back a response upon successful
+reception and delivery of the message. This is the mechanism to determine if we
+should resend a message. This also implies we need to handle duplicate messages
+on the receiver's side. 
+
+## Code flow
+- Execution on each replica starts by gaining knowledge of the cluster
+configuration (e.g. the other nodes in the system), setting up its initial
+default state, then beginning its execution loop. 
+- Instead of having 2 separate execution loops for primary and backup replicas,
+have a single execution loop that checks at the top if the replica is 
+currently a primary or a backup. You can abstract out the actual primary and 
+backup behavior into separate functions if that feels more organized.
+- Instead of programming the message receive behavior into the execution loops
+themselves, it may be cleaner to simply move that logic into the message handler
+functions--just have a check at the message handler level if the replica is a
+primary or a backup, then execute differently depending on the result of the
+check. This may also allow us to generalize the message reception portion of the
+execution loop code between primary and backup. Generally, moving logic out
+of the main loop and into message-specific handlers seems to make the code
+more flexible in the long run.
+- Attempt to generalize message send and receive behavior, since you'll need
+to add resend and duplicate management logic for all messages.
+  - For now, attempt resend 3 times. If no confirmation is received after 3
+  attempts, assume disconnection (either network or node failure).
+- What should be done when a node is considered disconnected?
+- What should be done if the next primary fails during a view change? How do we
+guarantee that the protocol will move on to the next possible primary?
+
+### Message de-deduplication in the view change protocol
+- De-duplication is hard to generalize--we don't want to store a history of all
+the messages ever passed to a node and certain messages may require different 
+de-duplication policies. For example, the handling of some messages may already
+be idempotent and not require any de-duplication. Other messages require 
+de-duplication, but only up to a certain point, after which the protocol will
+automatically discard the messages (e.g. if they are from a previous view). As
+a result, I think we should *do de-duplication in the message-specific handlers.*
+
+## Development plan
+- Write code and test in small increments. Once tests pass and you're reasonably
+sure the code that was written does what it should do, write up a spec for the
+dialyzer.
--- vr2.erl
+++ vr2.erl
@@ -2,7 +2,7 @@
 -include_lib("eunit/include/eunit.hrl").
 -export([start/1]).
 
--record(replica_state, {configuration,
+-record(state, {configuration,
                         replica_number,
                         view_number=0,
                         status="normal",
@@ -15,15 +15,37 @@
                         do_vc_count=0,
                         max_vc_commit_number=0,
                         max_do_vc_msg}).
+-type state() :: #state{}.
+
+-record(commit, {view_number, commit_number}).
+-type commit() :: #commit{}.
 
 -record(start_viewchange, {view_number, replica_number}).
+-type start_vc() :: #start_viewchange{}.
+
 -record(do_viewchange, {view_number, 
                         log, 
                         last_normal_view, 
                         op_number, 
                         commit_number, 
                         replica_number}).
+-type do_vc() :: #do_viewchange{}.
+
 -record(start_view, {view_number, log, op_number, commit_number}).
+-type start_v() :: #start_view{}.
+
+-type vc_msg() :: start_vc() | do_vc() | start_v().
+-type norm_msg() :: commit().
+
+-type msg() :: norm_msg() | vc_msg().
+
+%% =============================================================================
+%% Message handlers
+%% =============================================================================
+%% TODO: below
+handle_start_viewchange(State, Msg) -> void.
+
+handle_do_viewchange(State, Msg) -> void.
 
 %% =============================================================================
 %% Exported functions
@@ -38,35 +60,71 @@ start(CfgFile) ->
     end,
     %% Set up initial cluster state
     I = lookup_node_index(node(), Content, 0),
-    State = #replica_state{configuration=Content, replica_number=I},
+    State = #state{configuration=Content, replica_number=I},
     %% Enter execution loop
     loop(State).
 
 loop(State) ->
-    I = State#replica_state.replica_number,
-    V = State#replica_state.view_number,
+    I = State#state.replica_number,
+    V = State#state.view_number,
     case {I, V} of
-        {Same, Same} -> primary;
-        _ -> backup
+        {Same, Same} -> loop(primary(State));
+        _ -> loop(backup(State))
     end.
+
+primary(State) ->
+    V = State#state.view_number,
+    Commit = State#state.commit_number,
+    Msg = #commit{view_number=V, commit_number=Commit},
+    Cfg = State#state.configuration,
+    case send_msg(Msg, Cfg, 3) of
+        ok -> ok;
+        {error, Fails} -> io:format("Message failed to send to: ~p~n", Fails)
+    end,
+    receive
+        {From, StartVC} when is_record(StartVC, start_viewchange) -> 
+            receive_msg(From, StartVC, fun handle_start_viewchange/2, State);
+        {From, DoVC} when is_record(DoVC, do_viewchange) -> 
+            receive_msg(From, DoVC, fun handle_do_viewchange/2, State)
+    end.
+    
+backup(State) -> void.
 
 %% =============================================================================
 %% Message-passing framework functions
 %% =============================================================================
-send_message_unicast(Msg, Node, Retry) -> 
+
+%% @doc Sends a message to a list of nodes, retrying a specified number of times
+%% for each node if no ack response is received.
+-spec send_msg(msg(), list(node()), integer()) -> ok | {error, list(node())}.
+send_msg(Msg, [Node], Retries) -> 
     Node ! {node(), Msg},
     receive
         {From, ok} when From =:= Node -> ok
     after 500 ->
-        case Retry of
-            0 -> fail;
-            _ -> send_message_unicast(Msg, Node, Retry-1)
+        case Retries of
+            0 -> {error, [Node]};
+            _ -> send_msg(Msg, [Node], Retries-1)
         end
+    end;
+send_msg(Msg, Nodes, Retries) ->
+    Resp = list:map(fun(X) -> send_msg(Msg, [X], Retries) end, Nodes),
+    %% Build a list of nodes that failed to deliver the message
+    Fails = [ X || {error, [X]} <- Resp],
+    case Fails of
+        [] -> ok;
+        _L -> {error, Fails}
     end.
 
-send_message_multicast(Msg, Nodes, Retry) ->
-    list:map(fun(X) -> send_message_unicast(Msg, X, Retry) end, Nodes).
-    
+%% @doc Sends an ack response back after a message is received and delivered,
+%% then runs the specified handler of the message. Should be called within a
+%% receive block.
+-spec receive_msg(node(), msg(), fun((state(), msg()) -> state()), state()) -> state().
+receive_msg(From, Msg, Handler, State) ->
+    %% Return message delivered ack to sending node
+    From ! {node(), ok},
+    %% Run handler using Msg and State
+    Handler(State, Msg).
     
 
 %% =============================================================================
@@ -84,7 +142,7 @@ load_config(File) ->
         error -> {error, Content}
     end.
 
-%% @doc Lookup_node_index does a simple linear search of the list
+%% @doc Finds the index number of a node in the configuration
 %% NOTE: Can we do something more efficient, like a binary search?
 lookup_node_index(Node, [H|_], Cnt) when H =:= Node -> Cnt;
 lookup_node_index(Node, [_|T], Cnt) -> lookup_node_index(Node, T, Cnt+1).
