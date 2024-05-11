3332163a0af024c14fc4614821cfb4818e82469a
Lucas Du <lucaszdu@gmail.com>
2024-05-11 18:26:37 -0400

    Make KV store more robust to not exists errors

--- kv.erl
+++ kv.erl
@@ -1,5 +1,12 @@
 -module(kv).
 -behaviour(gen_server).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
+         code_change/3]).
+-export([start_link/0, create_entry/3, delete_entry/2, update_value/3,
+         get_value/2]).
+
+%% NOTE: From the documentation, if a callback function fails or returns a bad
+%%% value, the gen_server process terminates.
 
 start_link() ->
     gen_server:start_link(?MODULE, [], []).
@@ -19,6 +26,7 @@ get_value(Pid, Key) ->
 
 %% Server functions
 init([]) ->
+    io:format("I'm here!~n"),
     {ok, maps:new()}.
 
 handle_call({create, Key, Value}, _From, KV) ->
@@ -28,14 +36,26 @@ handle_call({delete, Key}, _From, KV) ->
     NewKV = maps:remove(Key, KV),
     {reply, deleted, NewKV};
 handle_call({update, Key, Value}, _From, KV) ->
-    NewKV = maps:update(Key, Value, KV),
-    {reply, {Key, Value}, NewKV};
+    %% Handle smoothly if value does not exist
+    try NewKV = maps:update(Key, Value, KV) of
+        _ ->
+            {reply, {Key, Value}, NewKV}
+    catch
+        error:{badkey, _Key} ->
+            {reply, badkey, KV}
+    end;
 handle_call({get, Key}, _From, KV) ->
-    Value = maps:get(Key, KV),
-    {reply, Value, KV}.
+    %% Handle smoothly if value does not exist
+    try Value = maps:get(Key, KV) of
+        _ ->
+            {reply, {Key, Value}, KV}
+    catch
+        error:{badkey, _Key} ->
+            {reply, badkey, KV}
+    end.
 
-handle_cast(_Msg, State) ->
-    {noreply, State}.
+handle_cast(_Msg, KV) ->
+    {noreply, KV}.
 handle_info(_Info, State) ->
     {noreply, State}.
 terminate(_Reason, _State) ->
